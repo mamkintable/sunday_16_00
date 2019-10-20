ac4fe55256c9bd4bf109a0df0df24570fc7dd742
splincode <cherkesov-artur@mail.ru>
2019-10-20 11:54:55 +0300

    homework

---
+++ lesson3/my_hm_Cherkesov/task_№2.py
@@ -0,0 +1,8 @@
+x1 = int(input("Введите x1: "))
+y1 = int(input("Введите y1: "))
+x2 = int(input("Введите x2: "))
+y2 = int(input("Введите y2: "))
+if (x1 + y1 + x2 + y2) % 2 == 0:
+    print('Yes')
+else:
+    print('No')
---
+++ lesson3/my_hm_Cherkesov/task_№3.py
@@ -0,0 +1,6 @@
+m = int(input("Введите целое m: "))
+n = int(input("Введите целое n: "))
+if (m//n)//2:
+    print (m//n)
+else:
+    print("m на n нацело не делится")
