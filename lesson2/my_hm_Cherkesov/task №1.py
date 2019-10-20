cf1b8d832c701e6c9a760181a7f17de81b609fb6
splincode <cherkesov-artur@mail.ru>
2019-10-20 11:28:47 +0300

    homework

---
+++ lesson2/my_hm_Cherkesov/task №1.py
@@ -0,0 +1,3 @@
+n = int(input("Введите число: "))
+print(n+1)
+print(n-1)
---
+++ lesson2/my_hm_Cherkesov/task №2.py
@@ -0,0 +1,4 @@
+num1 = int(input("Введите число 1: "))
+num2 = int(input("Введите число 2: "))
+num3 = int(input("Введите число 3: "))
+print((num1+num2)/num3)
