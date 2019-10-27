f3ab6855ae1700587ec7be0e61d458b75c2f3508
goodly1409 <56189253+goodly1409@users.noreply.github.com>
2019-10-27 10:42:47 +0300

    Update task_№3.py

--- lesson3/my_hm_Cherkesov/task_№3.py
+++ lesson3/my_hm_Cherkesov/task_№3.py
@@ -2,5 +2,7 @@ m = int(input("Введите целое m: "))
 n = int(input("Введите целое n: "))
 if (m//n)//2:
     print (m//n)
+        if m==n:
+            print(m//n)
 else:
     print("m на n нацело не делится")
