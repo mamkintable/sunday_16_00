e366fc57a0030f4890e9573ed9e6da69df3bff93
goodly1409 <56189253+goodly1409@users.noreply.github.com>
2019-10-27 10:54:55 +0300

    Update task №1.py

--- lesson2/my_hm_Cherkesov/task №1.py
+++ lesson2/my_hm_Cherkesov/task №1.py
@@ -1,3 +1,3 @@
-n = int(input("Введите число: "))
-print(n+1)
-print(n-1)
+n = int(input("Введите чмлсо: "))
+print("The next number for the number " + str(n) + " is " + str(n + 1) + ".")
+print("The previous number for the number " + str(n) + " is " + str(n - 1) + ".")
