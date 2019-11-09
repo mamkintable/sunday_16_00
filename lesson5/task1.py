442fc28b6fbf62fadd6a704ef023888bb260f482
Dmitry Sitnikov <dim4s14@gmail.com>
2019-11-09 03:55:04 +0300

    added lesson5 and HM

---
+++ lesson5/task1.py
@@ -0,0 +1,9 @@
+s = 0
+n = 0
+while s < 350:
+    x = int(input())
+    s = s + x
+    n = n + 1
+s = s - x
+n = n - 1
+print(s)
---
+++ lesson5/task3.py
@@ -0,0 +1,3 @@
+n = int(input())
+for i in range(100, n, -1):
+    print(i)
